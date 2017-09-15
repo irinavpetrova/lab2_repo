bc5002756bb03e1dc1251c410ae4f58ba2111588
irinavpetrova <31988866+irinavpetrova@users.noreply.github.com>
2017-09-15 16:06:17 +0300

    Add files via upload

--- dic_algs.py
+++ dic_algs.py
@@ -1,9 +1,13 @@
 ivan = {"name" : "ivan","age":34,"children":[{"name" : "vasja","age":16}] }
-shashya = {"name" : "shashya","age":50,"children":[{"name" : "ivan","age":20}] }
+sasha = {"name" : "sasha","age":50,"children":[{"name" : "ivan","age":20}, {"name" : "dima","age":22}] }
 michael = {"name" : "michael","age":60,"children":[{"name" : "vasja","age":31}] }
-emp = [ivan,shashya,michael]
-for x in emp:
-    for i in x["children"]:
-        if i["age"] >18:
-            print(x["name"])
-
+emp = [ivan,sasha,michael]
+def n(emp):
+    mas_emps=[]
+    for emp_el in emp:
+        for i in emp_el["children"]:
+            if i["age"] > 18:
+               mas_emps.append(emp_el["name"])
+               break
+    return mas_emps
+print (*n(emp))
